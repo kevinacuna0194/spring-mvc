e9f064d4b6da77c8e3678c77b92be8a1325786ae
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2025-08-29 15:05:53 -0300

    Rest Controller

---
+++ src/main/java/com/example/webapp/springboot_web/controller/UserRestController.java
@@ -0,0 +1,25 @@
+package com.example.webapp.springboot_web.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserRestController {
+
+    // Define tus endpoints aquí
+    @GetMapping("/detailsJSON")
+    public Map<String, Object> details() {
+
+        Map<String, Object> body = new HashMap<>();
+
+        body.put("title", "Detalles del Usuario");
+        body.put("description", "Aquí están los detalles del usuario.");
+        body.put("userId", 1);
+        body.put("userName", "Juan Pérez");
+        body.put("userEmail", "juan.perez@example.com");
+        return body;
+    }
+}
