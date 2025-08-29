d24ae4d6300ab96c15206a514ef5d85698c02348
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2025-08-29 14:37:18 -0300

    Pasar datos a la vista

--- src/main/java/com/example/webapp/springboot_web/controller/UserController.java
+++ src/main/java/com/example/webapp/springboot_web/controller/UserController.java
@@ -1,13 +1,21 @@
 package com.example.webapp.springboot_web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+
     // Define tus endpoints aquí
     @GetMapping("/details")
-    public String details() {
+    public String details(Model model) {
+
+        model.addAttribute("title", "Detalles del Usuario");
+        model.addAttribute("description", "Aquí están los detalles del usuario.");
+        model.addAttribute("userId", 1);
+        model.addAttribute("userName", "Juan Pérez");
+        model.addAttribute("userEmail", "juan.perez@example.com");
         return "details";
     }
 }
