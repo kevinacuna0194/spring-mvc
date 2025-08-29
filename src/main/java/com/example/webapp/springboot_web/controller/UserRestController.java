5a27b0bc322c3c12d28d066ccf278765ca3f9ab4
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2025-08-29 16:48:52 -0300

    Anoteación @RequestMapping

--- src/main/java/com/example/webapp/springboot_web/controller/UserRestController.java
+++ src/main/java/com/example/webapp/springboot_web/controller/UserRestController.java
@@ -4,13 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserRestController {
 
     // Define tus endpoints aquí
-    @GetMapping("/detailsJSON")
+    @GetMapping("/details")
     public Map<String, Object> details() {
 
         Map<String, Object> body = new HashMap<>();
