db11254bd04fbe59e6ecdef276d6ba653ccb7904
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2025-08-30 00:08:36 -0300

    La clase DTO

--- src/main/java/com/example/webapp/springboot_web/controller/UserRestController.java
+++ src/main/java/com/example/webapp/springboot_web/controller/UserRestController.java
@@ -1,34 +1,49 @@
 package com.example.webapp.springboot_web.controller;
 
-import java.util.HashMap;
-import java.util.Map;
+// import java.util.HashMap;
+// import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.webapp.springboot_web.models.User;
+import com.example.webapp.springboot_web.models.dto.UserDto;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/users") // Endpoint base para usuarios
 public class UserRestController {
 
     // Define tus endpoints aquí
-    @GetMapping("/details")
-    public Map<String, Object> details() {
-
+    @GetMapping("/details") // Endpoint para obtener detalles del usuario
+    public UserDto Details() {
+        UserDto userDto = new UserDto();
+        userDto.setId(1);
+        userDto.setTitle("Detalles del Usuario");
+        userDto.setDescription("Aquí están los detalles del usuario.");
         User user = new User("Juan Pérez", "juan.perez@example.com");
+        userDto.setUser(user);
 
-        Map<String, Object> body = new HashMap<>();
-
-        body.put("title", "Detalles del Usuario");
-        body.put("description", "Aquí están los detalles del usuario.");
-        body.put("userId", 1);
-        // body.put("userName", "Juan Pérez");
-        // body.put("userEmail", "juan.perez@example.com");
-        // body.put("userName", user.getName());
-        // body.put("userEmail", user.getEmail());
-        body.put("user", user);
-        return body;
+        return userDto;
     }
+
+    /*
+     * @GetMapping("/details")
+     * public Map<String, Object> details() {
+     * 
+     * User user = new User("Juan Pérez", "juan.perez@example.com");
+     * 
+     * Map<String, Object> body = new HashMap<>();
+     * 
+     * body.put("title", "Detalles del Usuario");
+     * body.put("description", "Aquí están los detalles del usuario.");
+     * body.put("userId", 1);
+     * // body.put("userName", "Juan Pérez");
+     * // body.put("userEmail", "juan.perez@example.com");
+     * // body.put("userName", user.getName());
+     * // body.put("userEmail", user.getEmail());
+     * body.put("user", user);
+     * return body;
+     * }
+     */
 }
---
+++ src/main/java/com/example/webapp/springboot_web/models/dto/UserDto.java
@@ -0,0 +1,43 @@
+package com.example.webapp.springboot_web.models.dto;
+
+import com.example.webapp.springboot_web.models.User;
+
+public class UserDto {
+
+    private int id;
+    private User user;
+    private String title;
+    private String description;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
